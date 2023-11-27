9cbffc7e11d440991eae512e9731ec8fea2768af
Slav Abent <slavabent2015@gmail.com>
2023-11-27 20:59:53 +0500

    add cover image

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 import { ThemeProvider } from "@/components/providers/theme.provider";
 import { ConvexClientProver } from "@/components/providers/convex.provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -36,17 +37,19 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProver>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            storageKey="notion-clone-theme"
-          >
-            <Toaster position="bottom-center"/>
-            <ModalProvider />
-            {children}
-          </ThemeProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="notion-clone-theme"
+            >
+              <Toaster position="bottom-center"/>
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProver>
       </body>
     </html>
--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -8,6 +8,7 @@ import { ImageIcon, SmileIcon, X } from "lucide-react";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import TextareaAutosize from 'react-textarea-autosize'
+import { useCoverImage } from "@/hooks/use-cover-image";
 
 interface ToolbarProps {
   initialData: Doc<"documents">
@@ -22,8 +23,10 @@ export const Toolbar = ({
   const [isEditing, setIsEditing] = useState(false)
   const [value, setValue] = useState(initialData.title)
 
-  const  update = useMutation(api.documents.update)
+  const update = useMutation(api.documents.update)
   const removeIcon = useMutation(api.documents.removeIcon)
+
+  const coverImage = useCoverImage()
 
   const enableInput = () => {
 	 if (preview) return
@@ -105,7 +108,7 @@ export const Toolbar = ({
 		  )}
 		  {!initialData.coverImage && !preview && (
 			 <Button
-				onClick={() => {}}
+				onClick={coverImage.onOpen}
 				className="text-muted-foreground text-xs"
 				variant="outline"
 				size="sm"
